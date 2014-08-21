4223ff6f233521bfba4b943c75af8a89de4dcf21
Pablo Gutierrez <pablo@vivalacloud.com>
2014-08-21 16:55:11 +0200

    issue174

--- server/ryadmin/ajax/brands/android/royappty/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/imac/development/royappty/server/ryadmin/ajax/brands/android/royappty/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/imac/development/royappty/server/ryadmin/ajax/brands/android/royappty/platforms/android/AndroidManifest.xml \
--- server/ryadmin/ajax/brands/android/royappty/platforms/android/ant-build/royappty-debug-unaligned.apk.d
+++
@@ -1,6 +0,0 @@
-/Users/imac/development/royappty/server/ryadmin/ajax/brands/android/royappty/platforms/android/ant-build/royappty-debug-unaligned.apk : \
-/Users/imac/development/royappty/server/ryadmin/ajax/brands/android/royappty/platforms/android/ant-build/royappty.ap_ \
-/Users/imac/development/royappty/server/ryadmin/ajax/brands/android/royappty/platforms/android/ant-build/classes.dex \
-/Users/imac/development/royappty/server/ryadmin/ajax/brands/android/royappty/platforms/android/CordovaLib/ant-build/classes.jar \
-/Users/imac/development/royappty/server/ryadmin/ajax/brands/android/royappty/platforms/android/libs/gcm.jar \
-/Users/imac/development/royappty/server/ryadmin/ajax/brands/android/royappty/platforms/android/libs/android-support-v13.jar \
