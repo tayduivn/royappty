28d68b196975f13716a2a66bdd512a7661835e38
Pablo Gutierrez <pablo@vivalacloud.com>
2014-08-26 13:52:00 +0200

    issue181

--- server/www/include/settings.php
+++ server/www/include/settings.php
@@ -1,15 +1,41 @@
 <?php
+/*********************************************************
+*
+* Author: Pablo Gutierrez Alfaro <pablo@royappty.com>
+* Last Edit: 26-08-2014
+* Version: 0.94
+*
+*********************************************************/
 
-$conf = array(
-	'bdtype' => 'mysql',
-	'bdserver' => 'localhost',
-	'bdport' => '',
-	'bd' => 'royappty',
-	'bduser' => 'root',
-	'bdpass' => '2CuW2St9',
-	'bdprefix' => ''
-);
-$url_server = "http://www.royappty.com/";
+$server_option='local';
+
+switch ($server_option){
+	case "local"
+		$conf = array(
+			'bdtype' => 'mysql',
+			'bdserver' => 'localhost',
+			'bdport' => '',
+			'bd' => 'royappty',
+			'bduser' => 'root',
+			'bdpass' => 'root',
+			'bdprefix' => ''
+		);
+		$url_server = "http://localhost:8888/royappty/";
+
+		break;
+	case "server"
+		$conf = array(
+			'bdtype' => 'mysql',
+			'bdserver' => 'localhost',
+			'bdport' => '',
+			'bd' => 'royappty',
+			'bduser' => 'root',
+			'bdpass' => '2CuW2St9',
+			'bdprefix' => ''
+		);
+		$url_server = "http://www.royappty.com/";
+		break;
+}
 
 $campaign_bd_type[2]="discount";
 $campaign_bd_type[1]="coupon";
