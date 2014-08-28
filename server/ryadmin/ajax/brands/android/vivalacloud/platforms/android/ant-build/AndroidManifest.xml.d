97313829bfa57ff25e2d40ff9f784b4dec897400
MiGatoSeneca <pablo@vivalacloud.com>
2014-08-28 18:48:55 +0200

    issue181-server

--- server/ryadmin/ajax/brands/android/vivalacloud/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/var/www/royappty/server/ryadmin/ajax/brands/android/vivalacloud/platforms/android/ant-build/AndroidManifest.xml : \
-/var/www/royappty/server/ryadmin/ajax/brands/android/vivalacloud/platforms/android/AndroidManifest.xml \
