9d10291695729f18d559ec7135a3a25f842b5945
MiGatoSeneca <pablo@vivalacloud.com>
2014-09-05 13:58:30 +0200

    issue231

--- server/ryadmin/ajax/brands/android/vivalacloud/platforms/android/ant-build/vivalacloud-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/var/www/royappty/server/ryadmin/ajax/brands/android/vivalacloud/platforms/android/ant-build/vivalacloud-debug-unaligned.apk : \
-/var/www/royappty/server/ryadmin/ajax/brands/android/vivalacloud/platforms/android/ant-build/vivalacloud.ap_ \
-/var/www/royappty/server/ryadmin/ajax/brands/android/vivalacloud/platforms/android/ant-build/classes.dex \
-/var/www/royappty/server/ryadmin/ajax/brands/android/vivalacloud/platforms/android/CordovaLib/ant-build/classes.jar \
--- server/ryadmin/ajax/brands/android/vivalacloud/platforms/android/ant-gen/com/royappty/vivalaclou/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.royappty.vivalaclou;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
