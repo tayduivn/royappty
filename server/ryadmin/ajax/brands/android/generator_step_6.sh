752bdbdbca84131d66903cd946e89a6152a02529
MiGatoSeneca <pablo@vivalacloud.com>
2014-09-24 19:07:31 +0200

    issue275

--- server/ryadmin/ajax/brands/android/generator_step_6.sh
+++ server/ryadmin/ajax/brands/android/generator_step_6.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo ./generator_step_6_sudo.sh $1 $2 $3
+sudp ./generator_step_6_sudo.sh $1 $2 $3
 echo "[35m[Android Generator 0.0.1][39m Copy Application base <span class='pull-right'>[<span style='color:black'>START</span>]</span> "
 echo "[35m[Android Generator 0.0.1][39m Push Media Download <span class='pull-right'>[<span style='color:green'>OK</span>]</span> "
