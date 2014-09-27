d8318d572f0c7ef4d7e605241a2a94a1e8f1211c
MiGatoSeneca <pablo@vivalacloud.com>
2014-09-27 20:44:38 +0200

    issue294

--- server/ryadmin/ajax/brands/ios/generator_step_6.sh
+++ server/ryadmin/ajax/brands/ios/generator_step_6.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo ./generator_step_6_sudo.sh $1 $2 $3
 echo "[35m[iOS Generaton 1.0.0][39m Copy Application base <span class='pull-right'>[<span style='color:black'>START</span>]</span> "
-echo "[35m[iOS Generaton 1.0.0][39m Push Media Download <span class='pull-right'>[<span style='color:green'>OK</span>]</span> "
+echo "[35m[iOS Generaton 1.0.0][39m Alert & Media Download <span class='pull-right'>[<span style='color:green'>OK</span>]</span> "
